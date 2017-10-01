c5c49a0b4e3cc9cd342427375edfca9759b72f65
Andrew Jones <ascjones@gmail.com>
2017-10-01 16:03:21 +0100

    WIP: match rules

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate csv;
 extern crate chrono;
 
+// #[macro_use]
+// extern crate serde_derive;
+// extern crate toml;
+
 use std::error::Error;
 use std::io;
 use std::process;
@@ -11,6 +15,22 @@ struct Transaction {
     date: NaiveDate,
     amount: f32,
     description: String,
+}
+
+struct Rule {
+    description: String,
+    account: String,
+}
+
+struct MatchingRules {
+    rules: Vec<Rule>
+}
+
+struct LedgerTransaction {
+    date: NaiveDate,
+    amount: f32,
+    description: String,
+    account: String,
 }
 
 fn readcsv(date_fmt: &str) -> Result<Vec<Transaction>, Box<Error>> {
@@ -25,10 +45,18 @@ fn readcsv(date_fmt: &str) -> Result<Vec<Transaction>, Box<Error>> {
         }).collect()
 }
 
-fn write_txs(account: &str, txs: Vec<Transaction>) -> Result<(), Box<Error>> {
+impl MatchingRules {
+    fn match_transaction(&self, tx: &Transaction) -> &Option<&Rule> {
+        let rule = &self.rules.iter().find(|rule| rule.description == tx.description);
+        rule
+    }
+}
+
+fn write_txs(account: &str, txs: Vec<LedgerTransaction>) -> Result<(), Box<Error>> {
     for tx in txs {
         println!("{} * {}", tx.date, tx.description);
-        println!("    {} £{:.2}", account, tx.amount);
+        println!("    {} £{:.2}", tx.account, tx.amount);
+        println!("    {} £-{:.2}", account, tx.amount);
         println!("");
     }
     Ok(())
@@ -38,9 +66,41 @@ fn main() {
     // options to be passed in
     let date_fmt = "%d/%m/%Y";
     let account = "Liabilities:Amex";
+    let rules = 
+        MatchingRules {
+            rules : vec! [
+                Rule { 
+                    description : "AMAZON UK RETAIL AMAZON.CO.UK".to_string(), 
+                    account : "Expenses:Amazon".to_string() 
+                }
+            ]
+        };
+
 
     let txs = readcsv(&date_fmt).unwrap();
-    write_txs(&account, txs);
+    let tx_rules =
+        txs.into_iter()
+            .map (|tx| {
+                let rule = rules.match_transaction(&tx);
+                (tx, rule)
+            });
+
+    let matched =
+        tx_rules
+            .filter_map(|(tx, rule)| {
+                rule.map(|r| {
+                    LedgerTransaction { 
+                        date : tx.date,
+                        amount : tx.amount,
+                        description : tx.description, 
+                        account : r.account 
+                    }
+                })
+            }).collect();
+
+    // todo: unmatched
+
+    write_txs(&account, matched);
     // if let Err(err) = readcsv(&date_fmt) {
     //     println!("error running readcsv: {}", err);
     //     process::exit(1);
